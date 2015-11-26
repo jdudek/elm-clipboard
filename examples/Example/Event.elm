e589249a9533bf193adb1fbcb73cb8e937c4d1c4
Jan Dudek <jd@jandudek.com>
2015-11-26 08:54:17 +0100

    Use nice tooltips from PrimerCSS in Event example

--- examples/Example/Event.elm
+++ examples/Example/Event.elm
@@ -2,22 +2,21 @@ module Example.Event where
 
 import Effects
 import Html exposing (Html, div, text, input, button, span)
-import Html.Attributes exposing (id, type', value, class)
+import Html.Attributes exposing (id, type', value, class, attribute)
+import Html.Events exposing (onMouseLeave)
 import Task exposing (Task, andThen, onError, succeed)
 
 import Clipboard
 import Clipboard.Attributes exposing (..)
-import Timeout exposing (Action(TimedOut))
 
 type alias Model =
   { copied : Bool
-  , timeout : Timeout.Model
   }
 
 type Action
   = NoOp
   | ClipboardSuccess Clipboard.Success
-  | Timeout Timeout.Action
+  | ClearCopied
 
 sampleText = "elm package install evancz/elm-http 2.0.0"
 
@@ -25,7 +24,6 @@ init =
   let
     model =
       { copied = False
-      , timeout = Timeout.init
       }
 
     fx =
@@ -39,29 +37,23 @@ init =
 update action model =
   case action of
     ClipboardSuccess e ->
-      let
-        (tModel, tFx) = Timeout.start 1000
+      ({ model | copied = True }, Effects.none)
 
-        model' =
-          { model | copied = True, timeout = tModel }
-      in
-        (model', Effects.map Timeout tFx)
-
-    Timeout TimedOut ->
+    ClearCopied ->
       ({ model | copied = False }, Effects.none)
-
-    Timeout a ->
-      let
-        (tModel, tFx) = Timeout.update a model.timeout
-      in
-        ({ model | timeout = tModel }, Effects.map Timeout tFx)
 
     NoOp ->
       (model, Effects.none)
 
 view address model =
   let
-    status = if model.copied then "Copied!" else ""
+    tooltipAttrs =
+      [ class "btn tooltipped tooltipped-s"
+      , attribute "aria-label" "Copied!"
+      ]
+
+    buttonWithTooltip attrs content =
+      button (attrs ++ tooltipAttrs) content
   in
     span [ class "input-group" ]
       [ input
@@ -69,12 +61,12 @@ view address model =
           , value sampleText
           ] []
       , span [ class "input-group-button" ]
-        [ button
-          [ clipboardText sampleText
-          , onClipboardSuccess address ClipboardSuccess
-          , class "btn"
-          ]
-          [ span [ class "octicon octicon-clippy" ] []
-          ]
+        [ (if model.copied then buttonWithTooltip else button)
+            [ clipboardText sampleText
+            , onClipboardSuccess address ClipboardSuccess
+            , onMouseLeave address ClearCopied
+            , class "btn"
+            ]
+            [ span [ class "octicon octicon-clippy" ] [] ]
         ]
       ]
